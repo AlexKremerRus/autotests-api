c557d4eb0456913dee6d597e29782961af52a5ef
Alexey Nekhoroshev <nekhorosheff.a.v@yandex.ru>
2025-08-15 16:33:10 +0300

    Практикуемся в написании API автотестов. Часть 1

--- tools/assertions/base.py
+++ tools/assertions/base.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, Sized
 
 def assert_status_code(actual:int, expected: int):
     assert actual == expected, f"Статус код некорректный. Ожидается {expected}, а получаем {actual}"
@@ -18,3 +18,7 @@ def assert_is_true(actual: Any, name: str):
         f'Incorrect value: "{name}". '
         f'Expected true value but got: {actual}'
     )
+
+
+def assert_length(actual: Sized, expected: Sized, name:str):
+    assert len(actual) == len(expected), f"Некорректная длина объекта {name}, Ожидаемая длина {len(expected)}, Фактическая длина {len(actual)} "
